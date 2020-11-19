c062dfc6ba63759e16b08b013c0586759a4d36db
kuan cheng <a6232241@g.pccu.edu.tw>
2020-11-19 22:46:00 +0800

    修改 99 乘法表 td bgColor、履歷表新增個人網站連結

--- assets/js/99mul.js
+++ assets/js/99mul.js
@@ -1,11 +1,22 @@
 let tbody = document.querySelector('tbody')
 
+let rgb = Math.pow(256, 2) - 1
+let rgbAvg = Math.ceil(255 / 9)
+
 for (let i = 1; i < 10; i++) {
-    let tr = document.createElement('tr')
-    for (let j = 1; j < 10; j++) {
-        let td = document.createElement('td')
-        td.innerHTML = `${j}*${i} = ${i * j}`
-        tr.appendChild(td)
-    }
-    tbody.appendChild(tr)
+  let tr = document.createElement('tr')
+
+  let beforeColor = Math.floor(Math.random() * (rgb - 256) + 256)
+  let afterColor = 255
+
+  for (let j = 1; j < 10; j++) {
+    let td = document.createElement('td')
+    td.innerHTML = `${j}*${i} = ${i * j}`
+
+    td.style.backgroundColor = `#${beforeColor.toString(16)}${afterColor.toString(16)}`
+    afterColor -= rgbAvg
+
+    tr.appendChild(td)
+  }
+  tbody.appendChild(tr)
 }
